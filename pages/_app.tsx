52c1aa26c1189cf1f0bab651ff98420635382f64
NFT Moon Metaverse <93310987+nftmoonmetaverse@users.noreply.github.com>
2023-04-05 23:44:22 +0300

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,7 @@ import type { AppProps } from "next/app";
 import "../styles/globals.css";
 
 // This is the chain your dApp will work on.
-const activeChain = "mainnet";
+const activeChain = "Mainnet";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
